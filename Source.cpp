14f264ba7b798a0c78da5c970ac81f691b4c08db
Ярослав <kazymysik000@mail.ru>
2023-09-07 19:59:54 +0300

    Add all tasks

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,9 @@
 
 
 void main() {
-    int n, m, choice, i, j;
+    int rows, cols, choice, i, j, choice2;
     int* arr = 0;
+    int min, max, index, sum;
     setlocale(LC_ALL, "RUS");
     srand(time(NULL));
     do {
@@ -22,38 +23,98 @@ void main() {
         scanf("%d", &choice);
         switch (choice) {
         case 1:
-            for (i = 0; i < n; i++) {  // цикл по строкам
-                for (j = 0; j < m; j++) { // цикл по столбцам
-                    printf("%5d ", *(arr + i * m + j)); // 5 знакомест под элемент массива
+            if (arr == NULL) {
+                printf("Сначала создайте массив!\n");
+                break;
+            }
+            for (i = 0; i < rows; i++) {  // цикл по строкам
+                for (j = 0; j < cols; j++) { // цикл по столбцам
+                    printf("%5d ", *(arr + i * cols + j)); // вывод элементов массива
                 }
                 printf("\n");
             }
-            free(arr);
             break;
         case 2:
-
+            if (arr == NULL) {
+                printf("Сначала создайте массив!\n");
+                break;
+            }
+            min = *(arr + 0 * cols + 0);
+            max = *(arr + 0 * cols + 0);
+            for (i = 0; i < rows; i++) { // цикл по строкам
+                for (j = 0; j < cols; j++) { // цикл по столбцам
+                    if (*(arr + i * cols + j) < min) {
+                        min = *(arr + i * cols + j);
+                    }
+                    if (*(arr + i * cols + j) > max) {
+                        max = *(arr + i * cols + j);
+                    }
+                }
+            }
+            printf("Минимальное число: %d\n", min);
+            printf("Максимальное число: %d\n", max);
             break;
         case 3:
-
+            if (arr == NULL) {
+                printf("Сначала создайте массив!\n");
+                break;
+            }
+            do {
+                printf("\n\tМеню\n");
+                printf("1. Посчитать сумму в столбце\n");
+                printf("2. Посчитать сумму в строке\n");
+                printf("3. Вернуться\n");
+                printf("Выберите действие: ");
+                scanf("%d", &choice2);
+                switch (choice2) {
+                case 1:
+                    sum = 0;
+                    printf("Выберите номер стобца: ");
+                    scanf("%d", &index);
+                    for (i = 0; i < rows; i++) {
+                        sum += *(arr + i * cols + index - 1);
+                    }
+                    printf("\nСумма равна: %d", sum);
+                    break;
+                case 2: 
+                    sum = 0;
+                    printf("Выберите номер строки: ");
+                    scanf("%d", &index);
+                    for (j = 0; j < cols; j++) {
+                        sum += *(arr + (index - 1) * cols + j);
+                    }
+                    printf("\nСумма равна: %d", sum);
+                    break;
+                case 3: 
+                    system("cls");
+                    break;
+                default:
+                    system("cls");
+                    printf("Некорректный выбор!\n");
+                    break;
+                }
+            } while (choice2 != 3);
             break;
         case 4:
             printf("Введите количество строк: ");
-            scanf("%d", &n);
+            scanf("%d", &rows);
             printf("Введите количество столбцов: ");
-            scanf("%d", &m);
-            arr = (int*)malloc(n * m * sizeof(int));
+            scanf("%d", &cols);
+            arr = (int*)malloc(rows * cols * sizeof(int));
             if (arr == NULL) {
                 printf("Не удалось выделить память!\n");
-                return;
+                break;
             }
-            for (j = 0; j < m * n; j++) {
-                arr[j] = rand();
+            for (i = 0; i < rows; i++)  { // цикл по строкам
+                for (j = 0; j < cols; j++)  { // цикл по столбцам
+                    *(arr + i * cols + j) = rand () % 10;
+                }
             }
+            printf("Массив создан!\n");
             break;
         case 5:
             system("cls");
             printf("До свидания!\n");
-            free(arr);
             break;
         default:
             system("cls");
